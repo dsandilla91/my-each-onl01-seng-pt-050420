b299cdfe396403539f21ed91a81ad89d47d9fb39
Deborah Santillan <s.debb@yahoo.com>
2020-05-17 02:47:38 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -3,6 +3,7 @@ collection = [1, 2, 3, 4]
 
 def my_each(collection)
   i = 0 
+<<<<<<< HEAD
   while (i < collection.length) do 
   yield collection[i]
   
@@ -13,3 +14,8 @@ end
   return collection
 end
 
+=======
+  
+  
+end
+>>>>>>> 1bb74948c4bd654c096b76a79e7185c9388f98c4
